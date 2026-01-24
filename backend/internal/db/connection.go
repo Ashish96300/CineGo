0e4e3361764ec33433e3a9d4ba08c35994baa98d
Ashish Sen <ashishsen2609@gmail.com>
2026-01-24 12:38:07 +0530

    initialised mongo db in docker

---
+++ backend/internal/db/connection.go
@@ -0,0 +1,30 @@
+
+package db
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectMongo() (*mongo.Client, error) {
+	uri := os.Getenv("MONGO_URI")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	log.Println("✅ MongoDB connected")
+	return client, nil
+}
