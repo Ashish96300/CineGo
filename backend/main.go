e8df64674702e4efb35fb08307c08cd8867c25cf
Ashish Sen <ashishsen2609@gmail.com>
2026-01-30 13:04:09 +0530

    main.go

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,18 @@
 package main
 
-import(
+import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main(){
-	fmt.Print("hello,GO")
+	router := gin.Default()
+
+	router.GET("/" ,func(c *gin.Context){
+		c.String(200 ,"server running yooo!!!")
+	})
+	if err := router.Run(":8000"); err!=nil{
+		fmt.Println("failed to start server",err);
+	}
 }
