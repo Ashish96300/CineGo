4a8c10a35ac363c5ea7ae220933c551783265c0e
Ashish Sen <ashishsen2609@gmail.com>
2026-01-31 13:11:53 +0530

    collection code

--- backend/internal/db/connection.go
+++ backend/internal/db/connection.go
@@ -1,37 +1,46 @@
-
 package db
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
-	"fmt"
-	
+
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
-	
-	
 )
 
-func ConnectMongo() (*mongo.Client, error) {
+var MongoClient *mongo.Client
+
+func ConnectMongo() error {
 	uri := os.Getenv("MONGO_URI")
 	if uri == "" {
-		return nil, fmt.Errorf("MONGO_URI is empty")
+		return fmt.Errorf("MONGO_URI is empty")
 	}
 
 	client, err := mongo.Connect(options.Client().ApplyURI(uri))
 	if err != nil {
-		return nil, fmt.Errorf("failed to create mongo client: %w", err)
+		return fmt.Errorf("failed to create mongo client: %w", err)
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	if err := client.Ping(ctx, nil); err != nil {
-		return nil, fmt.Errorf("could not ping database: %w", err)
+		return fmt.Errorf("could not ping database: %w", err)
 	}
 
+	MongoClient = client
 	log.Println("✅ MongoDB connected successfully")
-	return client, nil
+	return nil
+}
+
+func OpenCollection(collectionName string) *mongo.Collection {
+	dbName := os.Getenv("DATABASE_NAME")
+	if dbName == "" {
+		log.Fatal("DATABASE_NAME is empty")
+	}
+
+	return MongoClient.Database(dbName).Collection(collectionName)
 }
